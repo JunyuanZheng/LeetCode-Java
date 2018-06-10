de89e458204d510e166b0f60dea8f9b109d643fb
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-06-09 22:52:30 -0700

    Add Question 116
    Add Question 116

---
+++ src/Question116.java
@@ -0,0 +1,16 @@
+import share.TreeLinkNode;
+
+public class Question116 {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode start = root;
+        while (start != null) {
+            TreeLinkNode cur = start;
+            while (cur != null) {
+                if (cur.left != null) cur.left.next = cur.right;
+                if (cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+            start = start.left;
+        }
+    }
+}
