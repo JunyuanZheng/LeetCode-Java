c9d5dcd693522ac4ecb61224dfa4dc63e82d509f
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-06-13 10:30:09 -0700

    Add Question 198
    Add Question 198

---
+++ src/Question198.java
@@ -0,0 +1,12 @@
+public class Question198 {
+    public int rob(int[] nums) {
+        int prevMax = 0;
+        int currMax = 0;
+        for (int x : nums) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
